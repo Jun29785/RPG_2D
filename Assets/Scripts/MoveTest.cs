c6b3e466b5cb918da069b33fc03b064fc3d2285a
qkr0121 <wn950430@naver.com>
2023-05-24 21:11:08 +0900

    [update] 이동 구현 및 이동 제한 구현

--- Assets/Scripts/MoveTest.cs
+++ Assets/Scripts/MoveTest.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class MoveTest : MonoBehaviour
 {
@@ -6,27 +7,29 @@ public class MoveTest : MonoBehaviour
     float field_width;
     float field_height;
 
-    public Vector2 field_Left;
-    public Vector2 field_Right;
-    public Vector2 field_Up;
-    public Vector2 field_Down;
+    Vector2 field_Left;
+    Vector2 field_Right;
+    Vector2 field_Up;
+    Vector2 field_Down;
 
-    public float weight_Left;
-    public float weight_Right;
-    public float weight_Up;
-    public float weight_Down;
+    float weight_Left;
+    float weight_Right;
+    float weight_Up;
+    float weight_Down;
 
-    public DirectionW X_DirW = new DirectionW();
-    public DirectionW Y_DirW = new DirectionW();
+    DirectionW X_DirW = new DirectionW();
+    DirectionW Y_DirW = new DirectionW();
 
-    public Vector2 DirectionVector = Vector2.zero;
+    Vector2 DirectionVector = Vector2.zero;
 
     public Transform perpendicularTransform;
     [Header("Test")]
-    public LineRenderer x_line;
-    public LineRenderer y_line;
-    public LineRenderer vector_line;
-    public LineRenderer perpendicular_line;
+    [SerializeField] private LineRenderer x_line;
+    [SerializeField] private LineRenderer vector_line;
+    [SerializeField] private LineRenderer y_line;
+    [SerializeField] private LineRenderer perpendicular_line;
+
+    [SerializeField] private WaitForSeconds waitSetDirection = new WaitForSeconds(1.7f);
 
     public float speed = 2.5f;
     public bool IsMove;
@@ -45,7 +48,6 @@ public class MoveTest : MonoBehaviour
 
     void Update()
     {
-        //if (MapRangeCheck()) return;
         curMoveWaitDelay += Time.deltaTime;
         if (curMoveWaitDelay < maxMoveWaitDuration && IsMove)
         {
@@ -55,66 +57,41 @@ public class MoveTest : MonoBehaviour
         {
             IsMove = false;
             curMoveWaitDelay = 0f;
-            RandomMove();
+            StartCoroutine(RandomMove());
             IsMove = true;
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SetBorderValue();
-        }
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            RandomMove();
-        }
     }
-
-    bool MapRangeCheck()
+    
+    IEnumerator RandomMove()
     {
-        var x = transform.position.x;
-        var y = transform.position.y;
-        if (x > field_Right.x || x < field_Left.x || y > field_Up.y || y < field_Down.y)
-        {
-            Debug.Log($"x: {x} y: {y}");
-            return true;
-        }
-        return false;
-    }
-
-   void RandomMove()
-    {
+        DirectionVector = Vector2.zero;
+        yield return waitSetDirection;
         SetBorderValue();
         AllocDistance2Weight();
         SetWeight(ref weight_Left, ref weight_Right);
         SetWeight(ref weight_Up, ref weight_Down);
         X_DirW = SelectDirW(new DirectionW(field_Left, weight_Left), new DirectionW(field_Right, weight_Right));
         Y_DirW = SelectDirW(new DirectionW(field_Up, weight_Up), new DirectionW(field_Down, weight_Down));
-        //SelectDirW(ref X_DirW, new DirectionW(Vector2.left, weight_Left), new DirectionW(Vector2.right, weight_Right));
-        //SelectDirW(ref Y_DirW, new DirectionW(Vector2.up, weight_Up), new DirectionW(Vector2.down, weight_Down));
         DirectionVector = GetDirection().normalized;
-        x_line.SetPosition(0, field_Left);
-        x_line.SetPosition(1, field_Right);
-        y_line.SetPosition(0, field_Up);
-        y_line.SetPosition(1, field_Down);
-        vector_line.SetPosition(0, X_DirW.direction);
-        vector_line.SetPosition(1, Y_DirW.direction);
-        perpendicular_line.SetPosition(0, transform.position);
-        perpendicular_line.SetPosition(1, perpendicularTransform.position);
     }
 
     void SetBorderValue()
     {
-        field_Left = new Vector2(field.transform.position.x - (field_width / 2), transform.position.y);
-        field_Right = new Vector2(field.transform.position.x + (field_width / 2), transform.position.y);
-        field_Up = new Vector2(transform.position.x, field.transform.position.y + (field_height / 2));
-        field_Down = new Vector2(transform.position.x, field.transform.position.y - (field_height / 2));
+        float field_x = field.transform.position.x; float field_y = field.transform.position.y;
+        float x = transform.position.x; float y = transform.position.y;
+        field_Left = new Vector2(field_x - (field_width / 2), y);
+        field_Right = new Vector2(field_x + (field_width / 2), y);
+        field_Up = new Vector2(x, field_y + (field_height / 2));
+        field_Down = new Vector2(x, field_y - (field_height / 2));
     }
 
     void AllocDistance2Weight()
     {
-        weight_Left = Vector2.Distance(transform.position, field_Left);
-        weight_Right = Vector2.Distance(transform.position, field_Right);
-        weight_Up = Vector2.Distance(transform.position, field_Up);
-        weight_Down = Vector2.Distance(transform.position, field_Down);
+        var pos = transform.position;
+        weight_Left = Vector2.Distance(pos, field_Left);
+        weight_Right = Vector2.Distance(pos, field_Right);
+        weight_Up = Vector2.Distance(pos, field_Up);
+        weight_Down = Vector2.Distance(pos, field_Down);
     }
 
     void SetWeight(ref float arg1, ref float arg2)
@@ -126,25 +103,38 @@ public class MoveTest : MonoBehaviour
 
     DirectionW SelectDirW(DirectionW arg1, DirectionW arg2)
     {
-        if (Random.Range(0f, 1f) > arg1.weight)
-        {
+        if (arg1.weight >= 0.9)
+            return arg1;
+        if (arg1.weight <= 0.1)
             return arg2;
-        }
+        if (Random.Range(0f, 1f) > arg1.weight)
+            return arg2;
         else return arg1;
     }
 
     Vector2 GetDirection()
     {
+        var pos = transform.position;
         Vector2 c_Vector = X_DirW.direction - Y_DirW.direction;
-        float x_Length = X_DirW.direction.x - transform.position.x;
-        float y_Length = Y_DirW.direction.y - transform.position.y;
+        float x_Length = X_DirW.direction.x - pos.x;
+        float y_Length = Y_DirW.direction.y - pos.y;
         float scala = Mathf.Pow(y_Length, 2) / Mathf.Pow(c_Vector.magnitude, 2);
         Vector3 perpendicularPoint = new Vector2(x_Length * scala, y_Length * (1 - scala));
         perpendicularTransform.localPosition = perpendicularPoint;
-        return perpendicularTransform.position - transform.position;
+        return perpendicularTransform.position - pos;
     }
 
-
+    void TestCode()
+    {
+        x_line.SetPosition(0, field_Left);
+        x_line.SetPosition(1, field_Right);
+        y_line.SetPosition(0, field_Up);
+        y_line.SetPosition(1, field_Down);
+        vector_line.SetPosition(0, X_DirW.direction);
+        vector_line.SetPosition(1, Y_DirW.direction);
+        perpendicular_line.SetPosition(0, transform.position);
+        perpendicular_line.SetPosition(1, perpendicularTransform.position);
+    }
 
     //Vector2 CrossCheck(Vector2 a, Vector2 b, Vector2 c, Vector2 d)
     //{
@@ -163,6 +153,15 @@ public class MoveTest : MonoBehaviour
 
     //    return new Vector2(X, Y);
     //}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("wall"))
+        {
+            Debug.Log("Collision with wall Start RandomMove()");
+            StartCoroutine(RandomMove());
+        }
+    }
 }
 
 [System.Serializable]
