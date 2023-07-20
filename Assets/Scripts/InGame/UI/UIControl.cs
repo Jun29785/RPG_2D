7d59b530db4869a366986115efbdcfc822e7cb92
qkr0121 <wn950430@naver.com>
2023-07-20 21:51:25 +0900

    [update] Control Panel

---
+++ Assets/Scripts/InGame/UI/UIControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    public TeamSetting teamSetting;
+
+    public bool isOpen;
+
+    /// <summary>
+    /// 하단의 버튼 중 아무거나 클릭하게 되면 해당 이벤트를 처리 해줌
+    /// </summary>
+    public void OnClickButton()
+    {
+        
+    }
+}
