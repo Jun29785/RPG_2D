c52f5ad653174b2135c8f146995acf4381261e09
qkr0121 <wn950430@naver.com>
2023-07-26 21:48:49 +0900

    [add] boss

---
+++ Assets/Scripts/InGame/Enemy/Boss/BossBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 보스 : 맵 중앙보다 위쪽에 소환
+// 이동하지 않음, 전체적인 스텟이 높음, 드랍하는 아이템이 존재, 
+public class BossBase : MonoBehaviour
+{
+    public string bossName;
+    public int hp;
+    public int damage;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
