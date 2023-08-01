c0dd554760eac796be70296ee63c828a0d7752bc
qkr0121 <wn950430@naver.com>
2023-08-01 21:43:29 +0900

    [update] UserData

--- Assets/Scripts/Data/User/UserData.cs
+++ Assets/Scripts/Data/User/UserData.cs
@@ -1,13 +1,30 @@
 using System.Collections.Generic;
-using Define;
+using System;
 
-[System.Serializable]
+[Serializable]
 public class UserData
 {
     public string UserName;
 
     public int TeamLimit;
 
+    public SerializableDictionary<int, UnitData> unitDatas = new SerializableDictionary<int, UnitData>();
     public Dictionary<int, bool> unlockedUnits = new Dictionary<int, bool>();
     public Dictionary<int, bool> selectedUnits = new Dictionary<int, bool>();
+}
+
+[Serializable]
+public class UnitData
+{
+    public bool isUnlocked;
+    public bool isSelected;
+
+    #region Unit Stat Level
+    public int attack;
+    public int defense;
+    public int attackSpeed;
+    public int criticalProbability;
+    public int criticalAttack;
+    public int coolTime;
+    #endregion
 }
